19772ecff25c3fed170f954824983a6cbf02292f
DaianeM <daianersm2@gmail.com>
2021-07-08 23:13:24 -0300

    states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import cronometroImg from './assets/cronometro.png';
+
+import './index.css';
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      time: 0,
+      btnStatus: 'START'
+    };
+    
+    this.timer = null;
+    this.timeStart = this.timeStart.bind(this);
+    this.timeReset = this.timeReset.bind(this);
+  }
+
+  timeStart(){
+    let state = this.state;
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      state.btnStatus = 'START';
+    }else{
+      this.timer = setInterval(()=>{
+        let state = this.state;
+        state.time += 0.1;
+        this.setState(state);
+      },100)
+      this.state.btnStatus = 'PAUSE';
+    }
+
+    this.setState(state);
+  }
+
+  timeReset(){
+    
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    let state = this.state;
+    state.time = 0;
+    state.btnStatus = 'START'
+
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <img className="imgCronometro" src={cronometroImg} alt="cronometro"/>
+        <a className="timer">{this.state.time.toFixed(1)}</a>
+        <div className="areaBtn">
+          <a className="btn" onClick={this.timeStart}>{this.state.btnStatus}</a>
+          <a className="btn" onClick={this.timeReset}>RESET</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
